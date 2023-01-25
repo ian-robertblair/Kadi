cdff10ab84b0f69ec83d755abd2d6184db10ee85
ian-robertblair <ianrobertblair@icloud.com>
2023-01-25 11:46:28 +0800

    Initial Commit

---
+++ Kadi/KadiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KadiApp.swift
+//  Kadi
+//
+//  Created by ian robert blair on 2023/1/25.
+//
+
+import SwiftUI
+
+@main
+struct KadiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
